8734fecd79c3eb7defdad9b84531afbe92c26fe1
jportillo8 <buttokk@gmail.com>
2020-03-28 14:51:02 -0500

    Interfaz de Usuario - Creacion de Activities,Fragments y DialogFragments

---
+++ app/src/main/java/com/jportillo8/platziconf/ui/activities/SplashscreenActivity.kt
@@ -0,0 +1,13 @@
+package com.jportillo8.platziconf.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jportillo8.platziconf.R
+
+class SplashscreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}

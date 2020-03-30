e1c673e440e5e28aaaa672ca8620284e493529a4
jportillo8 <buttokk@gmail.com>
2020-03-30 15:16:24 -0500

    FireBase-Configuracion Establecida

--- app/src/main/java/com/jportillo8/platziconf/MainActivity.kt
+++ app/src/main/java/com/jportillo8/platziconf/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.jportillo8.platziconf
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setActionBar(findViewById(R.id.toolbarMain))
+        configNav()
+    }
+
+    fun configNav() {
+        NavigationUI.setupWithNavController(
+            bnvMenu,
+            Navigation.findNavController(this, R.id.fragContent)
+        )
     }
 }
---
+++ app/src/main/java/com/jportillo8/platziconf/model/Conference.kt
@@ -0,0 +1,5 @@
+package com.jportillo8.platziconf.model
+
+class Conference {
+
+}

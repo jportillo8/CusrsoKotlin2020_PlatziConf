c183810691d2e4de99aa1b1d013d55e6887b2335
jportillo8 <buttokk@gmail.com>
2020-03-30 15:24:39 -0500

    FireBase-Creados modelos conference speaker y ubication

--- app/src/main/java/com/jportillo8/platziconf/model/Conference.kt
+++ app/src/main/java/com/jportillo8/platziconf/model/Conference.kt
@@ -1,5 +1,11 @@
 package com.jportillo8.platziconf.model
 
+import java.util.*
+
 class Conference {
+    lateinit var tittle: String
+    lateinit var description: String
+    lateinit var tag: String
+    lateinit var datetime: Date
 
 }
---
+++ app/src/main/java/com/jportillo8/platziconf/model/Speaker.kt
@@ -0,0 +1,13 @@
+package com.jportillo8.platziconf.model
+
+class Speaker {
+    var name = ""
+    var joptitle = ""
+    var workplace = ""
+    var biography = ""
+    var twiter = ""
+    var image = ""
+
+
+
+}
